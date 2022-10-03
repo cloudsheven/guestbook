bfdfeb393767c4a4170e04d1734c7057dcde958c
andrea.luzzi <andrea.luzzi87@gmail.com>
2022-10-03 15:33:39 +0200

    starting gradebook project

---
+++ modules/my-gradebook/my-gradebook-web/src/main/java/com/tutorial/liferay/my/gradebook/web/constants/MyGradebookWebPortletKeys.java
@@ -0,0 +1,11 @@
+package com.tutorial.liferay.my.gradebook.web.constants;
+
+/**
+ * @author HP
+ */
+public class MyGradebookWebPortletKeys {
+
+	public static final String MYGRADEBOOKWEB =
+		"com_tutorial_liferay_my_gradebook_web_MyGradebookWebPortlet";
+
+}
---
+++ modules/my-gradebook/my-gradebook-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="mygradebookweb.caption"/></b>
+</p>
